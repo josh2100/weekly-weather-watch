fd89b4ec6cd88f793c1851dcb83c580b809d4f1a
joshgit <jschermann91@gmail.com>
2021-11-11 17:42:07 -0600

    api key working

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,19 @@ const textAreaEl = document.querySelector("textarea");
 const cityNameEl = document.querySelector("#cityName");
 const formEl = document.querySelector("#cityForm");
 const searchInput = document.querySelector("input");
-
 let cityQuery = "";
 const now = dayjs();
+
+// api key cdda6b75856c86ce1bea221c999009fa
+// API nightmares
+const asia =
+  "https://api.openweathermap.org/data/2.5/onecall?lat=45&lon=93&appid=cdda6b75856c86ce1bea221c999009fa";
+const chaiaIsAlwaysRight =
+  "https://api.openweathermap.org/data/2.5/onecall?lat=45&lon=93&exclude=minutely&appid=cdda6b75856c86ce1bea221c999009fa";
+var fiveDay =
+  "https://api.openweathermap.org/data/2.5/forecast?q=minneapolis&appid=cdda6b75856c86ce1bea221c999009fa&units=imperial";
+//   doc example api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt={cnt}&appid={API key}
+const dynamicApi = `https://api.openweathermap.org/data/2.5/forecast?q=${cityQuery}&appid=cdda6b75856c86ce1bea221c999009fa&units=imperial`;
 
 todayEl.textContent = now;
 
@@ -17,6 +27,52 @@ const searchCity = function (event) {
 
   cityQuery = searchInput.value.trim();
   console.log(cityQuery);
+
+  //   getWeather(cityQuery);
+
+  // update a var to represent city
+};
+
+const getWeather = function (api) {
+  // Make a request to the url
+  fetch(api)
+    .then(function (response) {
+      // request was successful
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          console.log(data);
+        });
+      } else {
+        // Use modal instead of alert
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function () {
+      alert("Unable to connect");
+    });
+};
+
+const getDynamicWeather = function () {
+  cityApi = `https://api.openweathermap.org/data/2.5/forecast?q=${cityQuery}&appid=cdda6b75856c86ce1bea221c999009fa&units=imperial`;
+
+  // Make a request to the url
+  fetch(cityApi)
+    .then(function (response) {
+      // request was successful
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          console.log(data);
+        });
+      } else {
+        // Use modal instead of alert
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function () {
+      alert("Unable to connect");
+    });
 };
 
 // Event listener for search button
