e1536809737cb6b2f07d817370d8f31d4c1b2d35
joshgit <jschermann91@gmail.com>
2021-11-17 14:20:48 -0600

    refactored out redundancy, update search box with recent items

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-// Start with displaying the day
-/// change or delete today el
 const cityNameEl = document.querySelector("#cityName");
 const formEl = document.querySelector("#cityForm");
+const leftSideContainerEl = document.querySelector("#left-side");
 const searchInput = document.querySelector("input");
+const recentSearchesEl = document.querySelector("#recent-searches");
 
 const currentTempEl = document.querySelector("#current-temp");
 const currentUviEl = document.querySelector("#current-uvi");
@@ -14,7 +14,7 @@ let latitude = "";
 let longitude = "";
 let recentSearches = [];
 
-const buildDate = function (addedDays) {
+const buildDate = (addedDays) => {
   //https://stackoverflow.com/questions/3572561/set-date-10-days-in-the-future-and-format-to-dd-mm-yyyy-e-g-21-08-2010
   const targetDate = new Date();
   targetDate.setDate(targetDate.getDate() + addedDays);
@@ -40,7 +40,7 @@ const searchCity = (event) => {
   getCurrentWeatherData();
 };
 
-const getCurrentWeatherData = function () {
+const getCurrentWeatherData = () => {
   let cityApi = `https://api.openweathermap.org/data/2.5/forecast?q=${cityQuery}&appid=cdda6b75856c86ce1bea221c999009fa&units=imperial`;
 
   // Make a request to the url
@@ -57,11 +57,10 @@ const getCurrentWeatherData = function () {
           cityNameEl.textContent = `${data.city.name} ${buildDate(0)}`;
           // Save successful search
           saveRecentSearches();
-
+          displayRecentSearches();
           getOneCallData();
         });
       } else {
-        // Use modal instead of alert
         alert("Error: " + response.statusText);
       }
     })
@@ -70,7 +69,7 @@ const getCurrentWeatherData = function () {
     });
 };
 
-const getOneCallData = function () {
+const getOneCallData = () => {
   let oneCallApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&appid=cdda6b75856c86ce1bea221c999009fa`;
 
   // Make a request to the url
@@ -79,37 +78,34 @@ const getOneCallData = function () {
       // Request was successful
       if (response.ok) {
         response.json().then(function (data) {
-          // console.log(data);
+          console.log(data);
+          // Insert Current Weather Data
           currentTempEl.textContent = `Current Temp: ${data.current.temp} °F`;
           currentUviEl.textContent = `UV Index: ${data.current.uvi}`;
           currentWindEl.textContent = `Wind: ${data.current.wind_speed} MPH`;
           currentHumidityEl.textContent = `Humidity: ${data.current.humidity}%`;
-
+          // Insert weather data for 5 day forecast
           for (let day = 0; day < 5; day++) {
+            document.querySelector(
+              `#day${day} div:nth-child(2)`
+            ).textContent = `${data.daily[day].weather[0].main}`;
+
             document.querySelector(
               `#day${day} div:nth-child(3)`
             ).textContent = `Temp: ${Math.round(data.daily[day].temp.day)} °F`;
-          }
 
-          for (let day = 0; day < 5; day++) {
             document.querySelector(
               `#day${day} div:nth-child(4)`
             ).textContent = `Wind: ${Math.round(
               data.daily[day].wind_speed
             )} MPH`;
-          }
 
-          for (let day = 0; day < 5; day++) {
             document.querySelector(
               `#day${day} div:nth-child(5)`
             ).textContent = `Humidity: ${Math.round(
               data.daily[day].humidity
             )}%`;
           }
-
-          //test section
-          console.log(data);
-          //test section
         });
       } else {
         alert("Error: " + response.statusText);
@@ -120,7 +116,7 @@ const getOneCallData = function () {
     });
 };
 
-var loadRecentSearches = function () {
+const loadRecentSearches = () => {
   city = JSON.parse(localStorage.getItem("city"));
 
   // if nothing in localStorage, create a new object to track all task status arrays
@@ -129,11 +125,9 @@ var loadRecentSearches = function () {
   } else {
     recentSearches = JSON.parse(localStorage.getItem("city"));
   }
-
-  console.log(city);
 };
 
-var saveRecentSearches = function () {
+const saveRecentSearches = () => {
   checkIfAlreadyAdded = function () {
     for (let index = 0; index < recentSearches.length; index++) {
       if (cityQuery == recentSearches[index]) {
@@ -143,18 +137,45 @@ var saveRecentSearches = function () {
   };
 
   if (recentSearches.some(checkIfAlreadyAdded)) {
-    console.log("already contains this!");
   } else {
+    // Add to beginning of list
     recentSearches.unshift(cityQuery);
   }
 
   localStorage.setItem("city", JSON.stringify(recentSearches));
 };
 
+const displayRecentSearches = () => {
+  // Clear out old content
+  recentSearchesEl.textContent = "";
+
+  for (let i = 0; i < 10; i++) {
+    if (recentSearches[i]) {
+      // Make a button
+      let newSearchButton = document.createElement("button");
+      // Give it the text of a search term in uppercase
+      let recentSearch = document.createTextNode(
+        recentSearches[i].toUpperCase()
+      );
+      // Add it to recent search section and add classes
+      newSearchButton.appendChild(recentSearch);
+      newSearchButton.classList.add("rounded", "p-1", "m-1");
+      recentSearchesEl.appendChild(newSearchButton);
+
+      // Add event listener to new search button
+      newSearchButton.addEventListener("click", function (event) {
+        searchInput.value = event.target.innerHTML.toLowerCase();
+      });
+    } else {
+      break;
+    }
+  }
+};
+
 // Event listener for search button
 cityForm.addEventListener("submit", searchCity);
 
-// Fill in today's data and 5 day forecast dates
+// Fill in todays date and 5 day forecast dates
 cityNameEl.textContent = buildDate(0);
 
 for (let day = 0; day < 5; day++) {
@@ -164,3 +185,4 @@ for (let day = 0; day < 5; day++) {
 }
 
 loadRecentSearches();
+displayRecentSearches();
